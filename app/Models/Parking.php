641117bc0c8c3a4679834d0d6803d69e59ad672e
Jefry Sánchez <sanchezmorajeffrey@yahoo.com>
2023-04-28 21:29:23 -0600

    add model for Parking, Vehicle, Zone add trait  HasUuids

---
+++ app/Models/Parking.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Parking extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = ['user_id', 'vehicle_id', 'zone_id', 'start_time', 'stop_time', 'total_price'];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'stop_time' => 'datetime',
+    ];
+}
---
+++ app/Models/Zone.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Zone extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = ['name', 'price_per_hour'];
+}
