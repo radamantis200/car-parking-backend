26dddec62bd9abdb198b136fb48703e7e356e6f7
Jefry Sánchez <sanchezmorajeffrey@yahoo.com>
2023-04-28 21:28:12 -0600

    add migrations for vehicles, parking, zones change column tokenable uuidMorphs

--- database/migrations/2023_04_28_203734_create_vehicles_table.php
+++ database/migrations/2023_04_28_203734_create_vehicles_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('vehicles', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignUuid('user_id')->constrained();
+            $table->string('plate_number');
+            $table->softDeletes();
 
             $table->timestamps();
         });
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('vehicles');
     }
 };
