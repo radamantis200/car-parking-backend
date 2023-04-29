a847580f85fb2bd30555b20548bcba58fa61d1b4
Jefry Sánchez <sanchezmorajeffrey@yahoo.com>
2023-04-29 09:53:16 -0600

    add route vehicle

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\Auth\LogoutController;
-use App\Http\Controllers\Api\V1\Auth\PasswordUpdateController;
 use App\Http\Controllers\Api\V1\Auth\ProfileController;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
+use App\Http\Controllers\Api\V1\Vehicle\VehicleController;
+use App\Http\Controllers\Api\V1\Auth\PasswordUpdateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,4 +27,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('profile', [ProfileController::class, 'show']);
     Route::put('profile', [ProfileController::class, 'update']);
     Route::put('password', PasswordUpdateController::class);
+
+    Route::apiResource('vehicle', VehicleController::class);
 });
