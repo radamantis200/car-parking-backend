a0b7c11d401c4acb1835a05ce6ceb857d54fccab
Jefry Sánchez <sanchezmorajeffrey@yahoo.com>
2023-04-29 09:53:03 -0600

    add softdelete for Vehicle add observer for Vehicle add VehicleObserver for AppServiceProvider

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -3,12 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Vehicle extends Model
 {
-    use HasFactory, HasUuids;
+    use HasFactory, HasUuids, SoftDeletes;
 
     protected $fillable = ['user_id', 'plate_number'];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('user', function (Builder $builder) {
+            $builder->where('user_id', auth()->id());
+        });
+    }
 }
