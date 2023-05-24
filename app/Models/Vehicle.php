9182e5aaa001eb1c73b7099dc408aa94aa9635fc
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2023-05-24 17:29:55 -0600

    change import

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
